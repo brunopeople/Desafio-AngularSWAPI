b593f898e9307978c6ab056b672944e56b0f4e5a
bruno pessoa nunes de melo <brunopessoa.melo@gmail.com>
2021-03-06 13:51:06 -0300

    instalando o modulo firebase, configurando o enviroment.ts e importando os modulos no appmodule.ts

--- desafio-starwars/src/app/app.module.ts
+++ desafio-starwars/src/app/app.module.ts
@@ -1,5 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {AngularFireModule} from "@angular/fire";
+import {AngularFireAuthModule} from "@angular/fire/auth";
+import { AngularFirestoreModule } from "@angular/fire/firestore";
+import { environment } from '../environments/environment';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
